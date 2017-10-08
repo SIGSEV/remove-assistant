814abcfdcde37e2ba9f7cfb6d36526ba155d5a0b
meriadec <meriadec.pillet@gmail.com>
2017-10-09 01:00:51 +0200

    use mobile site (idea of @Apercu)

--- src/track.js
+++ src/track.js
@@ -2,7 +2,8 @@ const watch = require('./watch')
 const screen = require('./screen')
 const save = require('./save')
 
-const getTweetURL = tweet => `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`
+const getTweetURL = tweet =>
+  `https://mobile.twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`
 
 module.exports = function track(user) {
   watch(user, async tweet => {
