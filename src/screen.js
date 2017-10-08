2309bf887321fdc425f35fe6b56b20c1f2f9221f
meriadec <meriadec.pillet@gmail.com>
2017-10-08 15:09:10 +0200

    ahah, screen ok lol

--- src/screen.js
+++ src/screen.js
@@ -1,3 +1,14 @@
+const puppeteer = require('puppeteer')
+
+const getTweetURL = tweet => `https://twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`
+
 module.exports = async function screen(tweet) {
-  console.log(`taking a screenshot of tweet ${tweet.id}`)
+  const browser = await puppeteer.launch({ args: ['--no-sandbox', '--disable-setuid-sandbox'] })
+  const page = await browser.newPage()
+  const url = getTweetURL(tweet)
+  await page.goto(url)
+  await page.screenshot({
+    path: 'hey.jpg',
+  })
+  await browser.close()
 }
