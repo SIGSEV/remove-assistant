4e5628dc4d0c8dffcfd3c101082a5c35358f36f7
meriadec <meriadec.pillet@gmail.com>
2017-10-09 00:55:50 +0200

    Revert "full page shot"
    This reverts commit 332bbc4d410d638479bd726be3d647f467b14c77.

--- src/screen.js
+++ src/screen.js
@@ -14,10 +14,7 @@ module.exports = async function screen(url) {
   const page = await b.newPage()
   await page.goto(url)
   const imgName = `${shortid.generate()}.jpg`
-  await page.screenshot({
-    path: path.join(SHOTS_DIR, imgName),
-    fullPage: true,
-  })
+  await page.screenshot({ path: path.join(SHOTS_DIR, imgName) })
   await page.close()
   return imgName
 }
