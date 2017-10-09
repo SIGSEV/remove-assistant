ec3c77b0f2c071e878ec32e79f0af9eef763f303
meriadec <meriadec.pillet@gmail.com>
2017-10-09 22:22:32 +0200

    far better screenshot

--- src/screen.js
+++ src/screen.js
@@ -11,12 +11,21 @@ module.exports = async function screen(url) {
     return
   }
   console.log(`>> taking a screenshot of ${url}`)
-  const page = await b.newPage()
-  await page.goto(url, {
-    waitUntil: 'networkidle',
-  })
   const imgName = `${shortid.generate()}.jpg`
-  await page.screenshot({ path: path.join(SHOTS_DIR, imgName) })
+  const page = await b.newPage()
+
+  await page.goto(url, { waitUntil: 'networkidle' })
+
+  await page.evaluate(() => {
+    const header = document.querySelector('[data-reactroot] header')
+    const eventualCookieFooter = document.querySelector('[data-reactroot] header + div')
+    if (eventualCookieFooter) {
+      eventualCookieFooter.parentNode.removeChild(eventualCookieFooter)
+    }
+    header.parentNode.removeChild(header)
+  })
+
+  await page.screenshot({ path: path.join(SHOTS_DIR, imgName), fullPage: true })
   await page.close()
   return imgName
 }
