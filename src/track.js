4891300ba771a2ae71dbbba32a0abaca73994f96
meriadec <meriadec.pillet@gmail.com>
2017-10-10 20:55:45 +0200

    retry streaming after failed connection

--- src/track.js
+++ src/track.js
@@ -5,7 +5,7 @@ const twitter = require('./twitter')
 const getTweetURL = tweet =>
   `https://mobile.twitter.com/${tweet.user.screen_name}/status/${tweet.id_str}`
 
-module.exports = function track() {
+module.exports = function track(retry = 5) {
   const stream = twitter.stream('user', {})
 
   stream.on('data', async tweet => {
@@ -36,6 +36,16 @@ module.exports = function track() {
   })
 
   stream.on('error', error => {
+    retry--
     console.log(`x Problem: ${error}`)
+    stream.destroy()
+    if (!retry) {
+      console.log(`x Stopping server (too many retries.,.)`)
+      process.exit(1)
+    }
+    console.log(`>> restarting... (remaining attempts: ${retry})`)
+    setTimeout(() => {
+      track(retry)
+    }, 2e3)
   })
 }
