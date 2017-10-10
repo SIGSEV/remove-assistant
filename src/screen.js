6db45d91b9272989cdc85c378257b1df656e49d0
meriadec <meriadec.pillet@gmail.com>
2017-10-10 22:04:39 +0200

    add timestamp and url to screenshot

--- src/screen.js
+++ src/screen.js
@@ -15,22 +15,7 @@ module.exports = async function screen(url) {
   // don't wait more than 3s for page load
   await page.goto(url, { waitUntil: 'networkidle', networkIdleTimeout: 3e3 })
 
-  const clip = await page.evaluate(() => {
-    const header = document.querySelector('[data-reactroot] header')
-    const eventualCookieFooter = document.querySelector('[data-reactroot] header + div')
-    if (eventualCookieFooter) {
-      eventualCookieFooter.parentNode.removeChild(eventualCookieFooter)
-    }
-    if (header) {
-      header.parentNode.removeChild(header)
-    }
-    const tweetElement = document.querySelector('[aria-label="Timeline: Conversation"] > div > div')
-    if (!tweetElement) {
-      return null
-    }
-    const rect = tweetElement.getBoundingClientRect()
-    return { x: rect.left, y: rect.top, width: rect.width, height: Math.min(rect.height, 1950) }
-  })
+  const clip = await page.evaluate(evaluate, url)
 
   if (!clip) {
     throw new Error('Cant find tweet node')
@@ -46,4 +31,43 @@ module.exports = async function screen(url) {
   await Promise.race([page.close(), wait(2e3)])
 
   return imgName
+}
+
+function evaluate(url) {
+  const header = document.querySelector('[data-reactroot] header')
+  const eventualCookieFooter = document.querySelector('[data-reactroot] header + div')
+  if (eventualCookieFooter) {
+    eventualCookieFooter.parentNode.removeChild(eventualCookieFooter)
+  }
+  if (header) {
+    header.parentNode.removeChild(header)
+  }
+
+  const tweetElement = document.querySelector('[aria-label="Timeline: Conversation"] > div > div')
+  if (!tweetElement) {
+    return null
+  }
+  tweetElement.style.position = 'relative'
+  tweetElement.style.paddingTop = '50px'
+
+  const dateElement = document.createElement('div')
+  dateElement.style.position = 'absolute'
+  dateElement.style.backgroundColor = 'yellow'
+  dateElement.style.top = 0
+  dateElement.style.left = 0
+  dateElement.innerHTML = new Date().toISOString()
+  tweetElement.appendChild(dateElement)
+
+  const urlElement = document.createElement('div')
+  urlElement.style.position = 'absolute'
+  urlElement.style.backgroundColor = 'yellow'
+  urlElement.style.top = `20px`
+  urlElement.style.left = 0
+  urlElement.innerHTML = url
+  tweetElement.appendChild(urlElement)
+
+  console.log(tweetElement.innerHTML)
+
+  const rect = tweetElement.getBoundingClientRect()
+  return { x: rect.left, y: rect.top, width: rect.width, height: Math.min(rect.height, 1950) }
 }
